1a4304882ea9d328fcddc58f3e1b75e7f8d2ab69
alifshitz <alifshitz@ebay.com>
2025-02-08 08:08:22 +0200

    Some api and schema changes

--- backend/app.py
+++ backend/app.py
@@ -12,26 +12,33 @@ db = SQLAlchemy(app)
 class run_result(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tool = db.Column(db.String(16), nullable=False, index=True)
-    data = db.Column(db.String(100), nullable=False)
     time = db.Column(db.Date)
+    json_data = db.Column(db.JSON)
+
+    def __repr__(self):
+        return f"<RunResult {self.id}, {self.tool}, {self.json_data}, {self.time}>"
 
 
 @app.route("/get_all_data", methods=["GET"])
 def get_data_per_tool():
     data = run_result.query.all()
-
     return jsonify(
-        [{"id": x.id, "tool": x.tool, "data": x.data, "time": x.time} for x in data]
+        [
+            {"id": x.id, "tool": x.tool, "time": x.time, "json_data": x.json_data}
+            for x in data
+        ]
     )
 
 
 @app.route("/add_data", methods=["POST"])
 def add_data():
     data = request.json
-    new_data = run_result(tool=data["tool"], data=data["data"], time=datetime.now())
+    new_data = run_result(
+        tool=data["tool"], time=datetime.now(), json_data=data.get("json_data", {})
+    )
     db.session.add(new_data)
     db.session.commit()
-    return {"id": new_data.id}
+    return {"id": new_data.id}, 201
 
 
 if __name__ == "__main__":
