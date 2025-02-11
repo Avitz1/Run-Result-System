b5ab74102f2d54df920fedc8d2fa4c82e2e3ae6e
Avitz1 <1avilifshitz@gmail.com>
2025-02-11 09:01:37 +0200

    Added backend filtering: tool, project, user and time

--- backend/routes.py
+++ backend/routes.py
@@ -7,20 +7,44 @@ from backend.services.schema_validations.schema_validator import validate, Valid
 main = Blueprint('main', __name__)
 
 
-@main.route("/get_all_data", methods=["GET"])
-def get_data_per_tool():
+@main.route("/get_filtered_data", methods=["GET"])
+def get_filtered_data():
     last_id = request.args.get('last_id', None, type=int)
     per_page = request.args.get('per_page', 1000, type=int)  # Default to 1000 rows per page
+    start_date = request.args.get('start_date', None)
+    end_date = request.args.get('end_date', None)
+    tool = request.args.get('tool', None)
+    username = request.args.get('username', None)
+    project = request.args.get('project', None)
 
-    query = RunResult.query.order_by(RunResult.id)
+    query = RunResult.query
+
+    if (start_date and not end_date) or (not start_date and end_date):
+        return jsonify({"error": "Both start_date and end_date must be provided."}), 400
+
     if last_id:
         query = query.filter(RunResult.id > last_id)
+    if start_date and end_date:
+        try:
+            start_date = datetime.strptime(start_date, "%Y-%m-%d")
+            end_date = datetime.strptime(end_date, "%Y-%m-%d")
+            query = query.filter(RunResult.time.between(start_date, end_date))
+        except ValueError:
+            return jsonify({"error": "Invalid date format. Use YYYY-MM-DD."}), 400
+    if username:
+        query = query.filter_by(username=username)
+    if tool:
+        query = query.filter_by(tool=tool)
+    if project:
+        query = query.filter_by(project=project)
+
+    query = query.order_by(RunResult.id)
 
     query = query.limit(per_page)
     data = query.all()
 
     return jsonify({
-        "data": [{"id": d.id, "tool": d.tool, "project": d.project, "time": d.time, "data": d.data} for d in data],
+        "data": [{"id": d.id, "tool": d.tool, "project": d.project, "time": d.time.isoformat(), "data": d.data} for d in data],
         "last_id": data[-1].id if data else None
     })
 
