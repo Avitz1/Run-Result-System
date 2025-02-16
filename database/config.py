cbbacc12262e5d7475c179278ebefcf78ca6b5fe
Avitz1 <1avilifshitz@gmail.com>
2025-02-16 14:25:15 +0200

    Adding absolute path for DB to avoid creation of 2 instances

--- database/config.py
+++ database/config.py
@@ -1,1 +1,5 @@
-DATABASE_URL = "sqlite:///run_results.db"
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_PATH = os.path.join(BASE_DIR, 'run_results.db')
+DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
